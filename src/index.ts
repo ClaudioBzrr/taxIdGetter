46e11b83f7a87026208aacd0722461271e90e45b
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-03-23 09:32:44 -0300

    added generate sheet with client errors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import Axios from 'axios'
 import {writeFile} from 'fs/promises'
 import path from 'path'
 
+const error_client:ClientProps[] = [] 
 const wb_file =  readFile(`${process.env.SHEET}`)
 const sheetName =  wb_file.SheetNames[0]
 const sheet = wb_file.Sheets[sheetName]
@@ -15,6 +16,9 @@ range.s.r = 1
 sheet['!ref'] = utils.encode_range(range)
 
 
+const wb_error = utils.book_new()
+const err_header = [['cnpj','name']]
+
 
 const client:ClientProps[] =  utils.sheet_to_json(sheet,{header,range})
 const api = (cnpj:string) => Axios.create({
@@ -24,15 +28,13 @@ const api = (cnpj:string) => Axios.create({
     },
     responseType:'stream'
 })
-// client.forEach(e =>console.log(`CNPJ : ${e.cnpj} - Nome : ${e.name}`))
-// console.log(`Total de ${client.length} registros`)
 
 
 async function generatePDF(cnpj:string,name:string,index:number){
     try{
         console.log(`Gerando pdf ${index+1} de ${client.length} ...`)
         const data = await api(cnpj).get('').then(response => response.data)
-        await writeFile(`${path.join(`${process.env.OUTPUT}`,`${name.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'')}.pdf`)}`,data).then(() =>console.log(`Pdf ${index+1} de ${client.length} Gerado`))
+        await writeFile(`${path.join(`${process.env.OUTPUT}`,`${cnpj} ${name.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'')}.pdf`)}`,data).then(() =>console.log(`Pdf ${index+1} de ${client.length} Gerado`))
     }catch(err){
         console.log(`${err}`)
     }
@@ -45,9 +47,15 @@ client.forEach(async ({cnpj,name},index) => {
         
         setTimeout( async () =>  {
             await generatePDF(cnpj,name,index)
-        },6000* (index+1))
+        },10000* (index+1))
             
     }catch(err){
+        error_client.push({cnpj,name})
         console.log(`${err}`)
     }
 })
+
+const ws_error =  utils.json_to_sheet(error_client)
+utils.sheet_add_aoa(ws_error,err_header)
+utils.book_append_sheet(wb_error,ws_error,'error')
+wf(wb_error,'client_error.xlsx')
